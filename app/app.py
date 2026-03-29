12a90961d5f988f2d55d149a2096285645af533d
as04-CyberNova <abhyudayasinha4@gmail.com>
2026-03-29 21:56:33 +0530

    Update app.py
    Added classification report display for model evaluation.

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ from sklearn.metrics import classification_report, accuracy_score, confusion_mat
 
 st.title("Titanic Survival Dashboard")
 
-df = pd.read_csv("D:\\DATA_SCIENCE_AND_CODING\\DATA_SCIENCE\\titanic_survival_dashboard_project\\data\\titanic.csv")
+df = pd.read_csv("data\titanic.csv")
 df = df.replace("?", pd.NA)
 
 st.sidebar.header("Filters")
